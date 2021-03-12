79a90b80488592a8dbf085b0ba6b8611b9ff90b4
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-03-12 16:24:43 +0530

    First commit

---
+++ SDET/src/test/java/practice/TestCase_01.java
@@ -0,0 +1,14 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class TestCase_01 {
+	
+	@Test
+	public void testcase_01() {
+		System.out.println("Step1");
+		System.out.println("Step2");
+		System.out.println("Step2");
+	}
+
+}
